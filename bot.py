8a626aab7c6532bc70e20b8c782d17b9dae4bc1b
sonya vishnevskaia <sofya.vishnevskaya0@yandex.ru>
2026-03-07 14:31:25 +0100

    start of refactoring

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 
+from config import BOT_TOKEN
+
 # Creating bot objects
-bot = Bot(token="6112328769:AAFd8CYRYtb-v05_bJFQahbDkUioBil_5Rw", parse_mode=ParseMode.HTML, timeout=60)
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML, timeout=60)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
